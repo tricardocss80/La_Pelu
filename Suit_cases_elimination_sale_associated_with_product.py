cd98d0eb15c2f52f38047f03c958abcfe15e6eb2
tricardocss80 <ricardonicolastasovac@gmail.com>
2020-06-25 09:39:05 -0300

    agregado de screanshops a try except

--- Suit_cases_elimination_sale_associated_with_product.py
+++ Suit_cases_elimination_sale_associated_with_product.py
@@ -13,7 +13,8 @@ from selenium.webdriver.chrome.options import Options
 class SalesCaseSuite(unittest.TestCase):
     def setUp(self):
         option = Options()
-        option.add_argument('start-maximized')
+        option.add_argument('--start-maximized')
+        #option.add_argument('--headless')
         self.driver = webdriver.Chrome('Chromedriver.exe', options=option)
         self.driver.get('https://www.lapelu.com.ar/')
         self.IndexPage = Pageindex(self.driver)
@@ -23,77 +24,74 @@ class SalesCaseSuite(unittest.TestCase):
         self.ClientPage = Pageclient(self.driver)
         self.EmployeesPage = Pageemployees(self.driver)
         self.FormsSalesPage = Pagesalesforms(self.driver)
-        data = {
-            'email': 'ricardonicolastasovac@gmail.com',
-            'password': 'mamapulpa'}
+        data = {'email': 'ricardonicolastasovac@gmail.com', 'password': 'mamapulpa'}
         self.IndexPage.Enterokay()
         self.LoginPage.login(data)
 
         # Crear un nuevo producto
     def test001_create_product(self):
-        data_product = {
-            'name': 'shampoo',
-            'cost': '100',
-            'price': '250',
-            'total': '5',
-            'minimum': '2',
-        }
-        self.ProductPage.menu_click_producto()
-        self.ProductPage.new_product()
-        self.ProductPage.form_new_product(data_product)
-        self.ProductPage.quantity_products()
-        quantity_products = self.ProductPage.quantity_products()
-        self.assertEqual(quantity_products, 1)
+        try:
+            data_product = {'name': 'shampoo', 'cost': '100', 'price': '250', 'total': '5', 'minimum': '2'}
+            self.ProductPage.menu_click_producto()
+            self.ProductPage.new_product()
+            self.ProductPage.form_new_product(data_product)
+            self.ProductPage.quantity_products()
+            quantity_products = self.ProductPage.quantity_products()
+            self.assertEqual(quantity_products, 1)
+        except:
+            self.driver.save_screenshot('error.png')
 
     # Crear un cliente
     def test002_create_client(self):
-        data_client = {
-            'nickname': 'Martita',
-            'name': 'Marta',
-            'surname': 'Lopez',
-            'email': 'martitalopez@gmail.com',
-            'phone': '1133551331',
-            'direction': 'Marcopolo 1013'
-        }
-        self.ClientPage.menu_click_client()
-        self.ClientPage.new_client()
-        self.ClientPage.form_client(data_client)
-        self.ClientPage.quantity_client()
-        quantity_clients = self.ClientPage.quantity_client()
-        self.assertEqual(quantity_clients, 1)
+        try:
+            data_client = {'nickname': 'Martita', 'name': 'Marta', 'surname': 'Lopez', 'email': 'martitalopez@gmail.com',
+                           'phone': '1133551331', 'direction': 'Marcopolo 1013'}
+            self.ClientPage.menu_click_client()
+            self.ClientPage.new_client()
+            self.ClientPage.form_client(data_client)
+            self.ClientPage.quantity_client()
+            quantity_clients = self.ClientPage.quantity_client()
+            self.assertEqual(quantity_clients, 5)
+        except:
+            self.driver.save_screenshot('error.png')
 
     # Crear un empleado
     def test003_crate_employees(self):
-        name = 'Victor Satriano'
-        self.EmployeesPage.menu_click_employees()
-        self.EmployeesPage.new_employees()
-        self.EmployeesPage.form_employees(name)
-        quantity_employees = self.EmployeesPage.quantity_employee()
-        self.assertEqual(quantity_employees, 1)
+        try:
+            name = 'Victor Satriano'
+            self.EmployeesPage.menu_click_employees()
+            self.EmployeesPage.new_employees()
+            self.EmployeesPage.form_employees(name)
+            quantity_employees = self.EmployeesPage.quantity_employee()
+            self.assertEqual(quantity_employees, 1)
+        except:
+            self.driver.save_screenshot('error.png')
 
     # Crear una venta con cliente, producto y empleado existentes
     def test004_Create_sale_with_existing_customer_product_employee(self):
-        data_sale = {
-            'client': 'mar',
-            'product': 'sha',
-            'quantity': '1'
-        }
-        self.SalesPage.sale_menu()
-        self.SalesPage.click_button_new_sale()
-        self.FormsSalesPage.form_new_sales(data_sale)
-        self.FormsSalesPage.sub_form_product(data_sale)
-        self.SalesPage.click_button_save_new_sale()
-        quantity_sales = self.SalesPage.quantity_sale()
-        self.assertEqual(quantity_sales, 1)
+        try:
+            data_sale = {'client': 'mar', 'product': 'sha', 'quantity': '1'}
+            self.SalesPage.sale_menu()
+            self.SalesPage.click_button_new_sale()
+            self.FormsSalesPage.form_new_sales(data_sale)
+            self.FormsSalesPage.sub_form_product(data_sale)
+            self.SalesPage.click_button_save_new_sale()
+            quantity_sales = self.SalesPage.quantity_sale()
+            self.assertEqual(quantity_sales, 1)
+        except:
+            self.driver.save_screenshot('error.png')
 
      #eliminar una venta asociada a un producto eliminado
     def test005_remove_sale_associated_with_removed_product(self):
-        self.SalesPage.sale_menu()
-        quantity_sales = self.SalesPage.quantity_sale()
-        self.SalesPage.click_delete_sale()
-        quantity_sales_delete = self.SalesPage.quantity_sale()
-        quantity_sales_total = quantity_sales - quantity_sales_delete
-        self.assertEqual(quantity_sales_total, 0)
+        try:
+            self.SalesPage.sale_menu()
+            quantity_sales = self.SalesPage.quantity_sale()
+            self.SalesPage.click_delete_sale()
+            quantity_sales_delete = self.SalesPage.quantity_sale()
+            quantity_sales_total = quantity_sales - quantity_sales_delete
+            self.assertEqual(quantity_sales_total, 0)
+        except:
+            self.driver.save_screenshot('error.png')
 
     def tearDown(self):
         self.driver.close()
